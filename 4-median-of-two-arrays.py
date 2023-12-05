e150b01e1e48683461b53ef4d2dd87ba9ac53902
Brian Breeden <bcbreeden@proton.me>
2023-12-05 12:20:08 -0500

    implemented floor division to reduce runtime

--- 4-median-of-two-arrays.py
+++ 4-median-of-two-arrays.py
@@ -2,9 +2,9 @@ def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
     combined = nums1 + nums2
     combined.sort()
     if len(combined) % 2 != 0:
-        return combined[int(len(combined)/2)]
+        return combined[len(combined)//2]
     else:
-        center = int(len(combined)/2)
+        center = len(combined)//2
         m1 = combined[center]
         m2 = combined[center-1]
         return (m1+m2)/2
