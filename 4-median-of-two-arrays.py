91888384da50b0cf40e7a1c5e076fc048a9e24dd
Brian Breeden <bcbreeden@proton.me>
2023-12-05 09:54:16 -0500

    Solved problem 4 median of two arrays

--- 4-median-of-two-arrays.py
+++ 4-median-of-two-arrays.py
@@ -1,5 +1,13 @@
 def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
     combined = nums1 + nums2
-    print(combined)
+    combined.sort()
+    if len(combined) % 2 != 0:
+        return combined[int(len(combined)/2)]
+    else:
+        center = int(len(combined)/2)
+        m1 = combined[center]
+        m2 = combined[center-1]
+        return (m1+m2)/2
 
-findMedianSortedArrays([1,3], [2])
+print(findMedianSortedArrays([1,3], [2]))
+print(findMedianSortedArrays([1,2], [3,4]))
